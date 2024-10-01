badb41fc8f786af88de593ca50a7841299694495
mlounello <mlounello@siena.edu>
2024-10-01 02:18:04 -0400

    Updated vrHandler.js to use LFS

--- js/vrHandler.js
+++ js/vrHandler.js
@@ -1,116 +1,119 @@
 function load360View(imageSrc) {
+    // Update the 360 image in the viewer
     document.getElementById('viewer-image').src = imageSrc;
 }
 
 function enterVR() {
+    // Enter VR mode (fullscreen)
     let viewerImage = document.getElementById('viewer-image');
     if (viewerImage.requestFullscreen) {
         viewerImage.requestFullscreen();
-    } else if (viewerImage.webkitRequestFullscreen) {
+    } else if (viewerImage.webkitRequestFullscreen) { // Safari
         viewerImage.webkitRequestFullscreen();
-    } else if (viewerImage.msRequestFullscreen) {
+    } else if (viewerImage.msRequestFullscreen) { // IE/Edge
         viewerImage.msRequestFullscreen();
-    } else if (viewerImage.mozRequestFullScreen) {
-        viewerImage.mozRequestFullScreen();
-    } else {
-        alert("Fullscreen API is not supported by your browser.");
-        return;
     }
-
     alert('Insert your phone into Google Cardboard.');
 }
 
-// Navigation Functions
 function showSubMenu(menuId) {
+    // Hide the main menu
     document.getElementById('main-menu').style.display = 'none';
+
+    // Hide all sub-menus
     let subMenus = document.getElementsByClassName('sub-menu');
     for (let i = 0; i < subMenus.length; i++) {
         subMenus[i].style.display = 'none';
     }
+
+    // Show the selected sub-menu
     document.getElementById(menuId).style.display = 'block';
 }
 
 function showMainMenu() {
+    // Hide all sub-menus
     let subMenus = document.getElementsByClassName('sub-menu');
     for (let i = 0; i < subMenus.length; i++) {
         subMenus[i].style.display = 'none';
     }
+
+    // Show the main menu
     document.getElementById('main-menu').style.display = 'block';
 }
 
-// Image Loading Functions (all lowercase)
-function loadryan() {
-    load360View("https://drive.google.com/uc?export=view&id=1b0JZ5Htz0S-fB-g2oLihWSeQn7VmlCdN");
-}
-
-function loadsnyder() {
-    load360View("https://drive.google.com/uc?export=view&id=1-jvQP43Ao55NYPyOlP59bXBMOiCoWpAz");
-}
-
-function loadupperdorm() {
-    load360View("https://drive.google.com/uc?export=view&id=1KhzUDtIkc2y8e-STKMcEQu6LCzqnoiP2");
-}
-
-function loadtownhouses() {
-    load360View("https://drive.google.com/uc?export=view&id=1sLuGhDas4tIzqqIVFG3CWYXezcH9E8EW");
+// The following functions load specific 360 images from the raw GitHub LFS URLs
+function loadbaldwin() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/baldwin.jpg");
 }
 
 function loadcaseys() {
-    load360View("https://drive.google.com/uc?export=view&id=1T1aKVgczQkjxnddZP6zY9mP5LG6mQDNI");
-}
-
-function loadlonnstromdining() {
-    load360View("https://drive.google.com/uc?export=view&id=1qLXEmV8U5nMeFKJcZjVcnUb3Whqcx_m0");
-}
-
-function loadlonnstrombuffet() {
-    load360View("https://drive.google.com/uc?export=view&id=1g5pQeRvI8LCJqrUNcCSlozGlM2nCs5u7");
-}
-
-function loadmorelllab() {
-    load360View("https://drive.google.com/uc?export=view&id=1kxJFIYICX4hdBBarPDdm6jqfuoTpbxSi");
-}
-
-function loadmorell() {
-    load360View("https://drive.google.com/uc?export=view&id=1ZfUCD0paEHSz7v5tRsQ2wqRReQndjGE5");
-}
-
-function loadbaldwin() {
-    load360View("https://drive.google.com/uc?export=view&id=1VKmXEqAFdgsn-FkW5XjwZ6vKBaDWP2aq");
-}
-
-function loadhickeytrading() {
-    load360View("https://drive.google.com/uc?export=view&id=1-jvQP43Ao55NYPyOlP59bXBMOiCoWpAz");
-}
-
-function loadmootcourt() {
-    load360View("https://drive.google.com/uc?export=view&id=1SXSpdeatH0fLdvOfR2RwDjVw8kdDZkMO");
-}
-
-function loadsienahall() {
-    load360View("https://drive.google.com/uc?export=view&id=16JtaWpSUtFnqtkm1dWGg5lp-IVAcHbS2");
-}
-
-function loadstackcenter() {
-    load360View("https://drive.google.com/uc?export=view&id=10X6KfTMYHx6qaMxeKP9m0SgEUsk53BRN");
-}
-
-function loadstandish() {
-    load360View("https://drive.google.com/uc?export=view&id=1JobIOUZF5o1i-csix9ZfRAy6DG-4DAYb");
-}
-
-function loadfitnessup() {
-    load360View("https://drive.google.com/uc?export=view&id=1BBXm7CG9C5NTOBODKI8HmJNeJFlKcqkh");
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/caseys.jpg");
 }
 
 function loadfitnessdown() {
-    load360View("https://drive.google.com/uc?export=view&id=1WDfpnDXzOEiG2VvIRuAOt3oyUvm9e6oE");
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/fitnessdown.jpg");
+}
+
+function loadfitnessup() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/fitnessup.jpg");
 }
 
 function loadhickeyfield() {
-    load360View("https://drive.google.com/uc?export=view&id=1c7G_xkmh1iaffRSNI_w_rrdHB-qbss6y");
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/hickeyfield.jpg");
+}
+
+function loadhickeytrading() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/hickeytrading.jpg");
+}
+
+function loadlonnstrombuffet() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/lonnstrombuffet.jpg");
+}
+
+function loadlonnstromdining() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/lonnstromdining.jpg");
+}
+
+function loadmootcourt() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/mootcourt.jpg");
+}
+
+function loadmorell() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/morell.jpg");
+}
+
+function loadmorelllab() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/morelllab.jpg");
+}
+
+function loadryan() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/ryan.jpg");
+}
+
+function loadsienahall() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/sienahall.jpg");
+}
+
+function loadsnyder() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/snyder.jpg");
+}
+
+function loadstackcenter() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/stackcenter.jpg");
+}
+
+function loadstandish() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/standish.jpg");
+}
+
+function loadtownhouses() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/townhouses.jpg");
 }
 
 function loadtvstudio() {
-    load360View("https://drive.google.com/uc?export=view&id=1Modm5JB8t9uRmJwxamjkJhflMEruhQjt");
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/tvstudio.jpg");
+}
+
+function loadupperdorm() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/upperdorm.jpg");
 }
