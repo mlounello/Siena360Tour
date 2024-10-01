3dbe228f463730d9868775d9cd92b06d42b8ab32
mlounello <mlounello@siena.edu>
2024-10-01 02:03:57 -0400

    Updated vrHandler.js to use Google Drive links

--- js/vrHandler.js
+++ js/vrHandler.js
@@ -1,10 +1,8 @@
 function load360View(imageSrc) {
-    // Update the 360 image in the viewer
     document.getElementById('viewer-image').src = imageSrc;
 }
 
 function enterVR() {
-    // Enter VR mode (fullscreen)
     let viewerImage = document.getElementById('viewer-image');
     if (viewerImage.requestFullscreen) {
         viewerImage.requestFullscreen();
@@ -16,104 +14,78 @@ function enterVR() {
     alert('Insert your phone into Google Cardboard.');
 }
 
-function showSubMenu(menuId) {
-    // Hide the main menu
-    document.getElementById('main-menu').style.display = 'none';
-
-    // Hide all sub-menus
-    let subMenus = document.getElementsByClassName('sub-menu');
-    for (let i = 0; i < subMenus.length; i++) {
-        subMenus[i].style.display = 'none';
-    }
-
-    // Show the selected sub-menu
-    document.getElementById(menuId).style.display = 'block';
-}
-
-function showMainMenu() {
-    // Hide all sub-menus
-    let subMenus = document.getElementsByClassName('sub-menu');
-    for (let i = 0; i < subMenus.length; i++) {
-        subMenus[i].style.display = 'none';
-    }
-
-    // Show the main menu
-    document.getElementById('main-menu').style.display = 'block';
-}
-
-// The following functions are for loading the specific 360 images for each location
-function loadRyan() {
-    load360View("assets/ryan.jpg");
-}
-
-function loadSnyder() {
-    load360View("assets/snyder.jpg");
-}
-
-function loadUpperDorm() {
-    load360View("assets/upperdorm.jpg");
-}
-
 function loadTownhouses() {
-    load360View("assets/townhouses.jpg");
-}
-
-function loadCaseys() {
-    load360View("assets/caseys.jpg");
-}
-
-function loadLonnstromDining() {
-    load360View("assets/lonnstromdining.jpg");
+    load360View("https://drive.google.com/uc?export=view&id=1sLuGhDas4tIzqqIVFG3CWYXezcH9E8EW");
 }
 
 function loadLonnstromBuffet() {
-    load360View("assets/lonnstrombuffet.jpg");
-}
-
-function loadMorellLab() {
-    load360View("assets/morelllab.jpg");
-}
-
-function loadMorell() {
-    load360View("assets/morell.jpg");
-}
-
-function loadBaldwin() {
-    load360View("assets/baldwin.jpg");
-}
-
-function loadHickeyTrading() {
-    load360View("assets/hickeytrading.jpg");
-}
-
-function loadMootCourt() {
-    load360View("assets/mootcourt.jpg");
-}
-
-function loadSienaHall() {
-    load360View("assets/sienahall.jpg");
-}
-
-function loadStackCenter() {
-    load360View("assets/stackcenter.jpg");
-}
-
-function loadStandish() {
-    load360View("assets/standish.jpg");
-}
-
-function loadFitnessUp() {
-    load360View("assets/fitnessup.jpg");
-}
-
-function loadFitnessDown() {
-    load360View("assets/fitnessdown.jpg");
+    load360View("https://drive.google.com/uc?export=view&id=1g5pQeRvI8LCJqrUNcCSlozGlM2nCs5u7");
 }
 
 function loadHickeyField() {
-    load360View("assets/hickeyfield.jpg");
+    load360View("https://drive.google.com/uc?export=view&id=1c7G_xkmh1iaffRSNI_w_rrdHB-qbss6y");
+}
+
+function loadCaseys() {
+    load360View("https://drive.google.com/uc?export=view&id=1T1aKVgczQkjxnddZP6zY9mP5LG6mQDNI");
+}
+
+function loadMootCourt() {
+    load360View("https://drive.google.com/uc?export=view&id=1SXSpdeatH0fLdvOfR2RwDjVw8kdDZkMO");
 }
 
 function loadTVStudio() {
-    load360View("assets/tvstudio.jpg");
+    load360View("https://drive.google.com/uc?export=view&id=1Modm5JB8t9uRmJwxamjkJhflMEruhQjt");
+}
+
+function loadStackCenter() {
+    load360View("https://drive.google.com/uc?export=view&id=10X6KfTMYHx6qaMxeKP9m0SgEUsk53BRN");
+}
+
+function loadHickeyTrading() {
+    load360View("https://drive.google.com/uc?export=view&id=1-jvQP43Ao55NYPyOlP59bXBMOiCoWpAz");
+}
+
+function loadSnyder() {
+    load360View("https://drive.google.com/uc?export=view&id=1-jvQP43Ao55NYPyOlP59bXBMOiCoWpAz");
+}
+
+function loadLonnstromDining() {
+    load360View("https://drive.google.com/uc?export=view&id=1qLXEmV8U5nMeFKJcZjVcnUb3Whqcx_m0");
+}
+
+function loadMorellLab() {
+    load360View("https://drive.google.com/uc?export=view&id=1kxJFIYICX4hdBBarPDdm6jqfuoTpbxSi");
+}
+
+function loadRyan() {
+    load360View("https://drive.google.com/uc?export=view&id=1b0JZ5Htz0S-fB-g2oLihWSeQn7VmlCdN");
+}
+
+function loadMorell() {
+    load360View("https://drive.google.com/uc?export=view&id=1ZfUCD0paEHSz7v5tRsQ2wqRReQndjGE5");
+}
+
+function loadFitnessDown() {
+    load360View("https://drive.google.com/uc?export=view&id=1WDfpnDXzOEiG2VvIRuAOt3oyUvm9e6oE");
+}
+
+function loadBaldwin() {
+    load360View("https://drive.google.com/uc?export=view&id=1VKmXEqAFdgsn-FkW5XjwZ6vKBaDWP2aq");
+}
+
+function loadUpperDorm() {
+    load360View("https://drive.google.com/uc?export=view&id=1KhzUDtIkc2y8e-STKMcEQu6LCzqnoiP2");
+}
+
+function loadStandish() {
+    load360View("https://drive.google.com/uc?export=view&id=1JobIOUZF5o1i-csix9ZfRAy6DG-4DAYb");
+}
+
+function loadFitnessUp() {
+    load360View("https://drive.google.com/uc?export=view&id=1BBXm7CG9C5NTOBODKI8HmJNeJFlKcqkh");
+}
+
+function loadSienaHall() {
+    load360View("https://drive.google.com/uc?export=view&id=16JtaWpSUtFnqtkm1dWGg5lp-IVAcHbS2");
 }
