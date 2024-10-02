b32b9f3e0717715fe42ca71e36e76f5c754c8337
mlounello <mlounello@siena.edu>
2024-10-02 00:15:38 -0400

    Code update

--- js/vrHandler.js
+++ js/vrHandler.js
@@ -66,6 +66,14 @@ function loadcaseys() {
     load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/caseys.jpg");
 }
 
+function loadlonnstromdining() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/lonnstromdining.jpg");
+}
+
+function loadlonnstrombuffet() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/lonnstrombuffet.jpg");
+}
+
 function loadsnyder() {
     load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/snyder.jpg");
 }
@@ -74,63 +82,16 @@ function loadryan() {
     load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/ryan.jpg");
 }
 
-// Add more location loading functions for other categories like dining, athletics, and academics.
-function loadfitnessdown() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/fitnessdown.jpg");
-}
-
 function loadfitnessup() {
     load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/fitnessup.jpg");
+}
+
+function loadfitnessdown() {
+    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/fitnessdown.jpg");
 }
 
 function loadhickeyfield() {
     load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/hickeyfield.jpg");
 }
 
-function loadhickeytrading() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/hickeytrading.jpg");
-}
-
-function loadlonnstrombuffet() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/lonnstrombuffet.jpg");
-}
-
-function loadlonnstromdining() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/lonnstromdining.jpg");
-}
-
-function loadmootcourt() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/mootcourt.jpg");
-}
-
-function loadmorell() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/morell.jpg");
-}
-
-function loadmorelllab() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/morelllab.jpg");
-}
-
-function loadsienahall() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/sienahall.jpg");
-}
-
-function loadstackcenter() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/stackcenter.jpg");
-}
-
-function loadstandish() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/standish.jpg");
-}
-
-function loadtownhouses() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/townhouses.jpg");
-}
-
-function loadtvstudio() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/tvstudio.jpg");
-}
-
-function loadupperdorm() {
-    load360View("https://media.githubusercontent.com/media/mlounello/Siena360Tour/refs/heads/main/assets/upperdorm.jpg");
-}
+// Add more functions for other locations as needed (Academics, Dining, etc.)
